be94b41b21810af6054a77cd7033a8c99564b8c0
Ahmadalmatari <aalmatari@neoleap.com.sa>
2022-12-28 18:41:43 +0300

    Clean the code

--- src/main/java/com/example/retailcashier/controllers/DiscountController.java
+++ src/main/java/com/example/retailcashier/controllers/DiscountController.java
@@ -2,14 +2,10 @@ package com.example.retailcashier.controllers;
 
 import com.example.retailcashier.models.*;
 import com.example.retailcashier.servieces.DiscountService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class DiscountController {
@@ -23,23 +19,7 @@ public class DiscountController {
     @PostMapping("/check-out")
     public Object checkOut(@RequestBody Cart cart) {
 
-        System.out.println(cart.getUser());
-        System.out.println(cart.getProducts());
-//        return cart;
         return discountService.checkOut(cart);
-
     }
 
-    @GetMapping("/")
-    public Cart test() {
-        List<Products> test = new ArrayList<>();
-
-
-        test.add(new Products("1", "t1", 1.2));
-        test.add(new Products("2", "t2", 2.2));
-
-        User user = new User("1", "ahmad", CustomerType.EMPLOYEE);
-        Cart cart = new Cart(user, test);
-        return cart;
-    }
 }
